edd47cde184b5cdc457220d821c1eb14e93fe519
Paul Murrell <paul@stat.auckland.ac.nz>
2018-10-18 13:55:13 +1300

    fix 'range' argument for Bezier splines (consisting of more than one Bezier curve)

--- R/bezier.R
+++ R/bezier.R
@@ -20,95 +20,75 @@ nSteps <- function(n) {
     }
 }
 
-bezCurveSlope <- function(x, y, stepFn=nSteps(100)) {
-    t <- stepFn(x, y)
-    t2 <- t*t
-    tmatrix <- cbind(1, t, t2)
-    mult <- tmatrix %*% derivMatrix
-    dx <- mult %*% x
-    dy <- mult %*% y
-    cbind(x=dx, y=dy)
-}
-
-bezSplineSlope <- function(x, y, stepFn=nSteps(100)) {
-    ncurves <- (length(x) - 1) %/% 3
-    if (ncurves*3 + 1 != length(x)) 
-        stop("Invalid number of control points")
-    ## Expand 'x' and 'y' to sets of four control points
-    index <- lapply(1:ncurves, function(i) ((i-1)*3 + 1):(i*3 + 1))
-    slopes <- lapply(1:ncurves,
-                     function(i) {
-                         index <- ((i-1)*3 + 1):(i*3 + 1)
-                         if (i == 1) {
-                             bezCurveSlope(x[index], y[index], stepFn)
-                         } else {
-                             bezCurveSlope(x[index], y[index], stepFn)[-1,]
-                         }
-                     })
-    list(x=unlist(lapply(slopes, function(c) c[,1])),
-         y=unlist(lapply(slopes, function(c) c[,2])))
-}
-
-BezierTangent <- function(x) {
-    xx <- convertX(x$x, "in", valueOnly=TRUE)
-    yy <- convertY(x$y, "in", valueOnly=TRUE)
-    nx <- length(xx)
-    ny <- length(yy)
-    n <- max(nx, ny)
-    if (nx != ny) {
-        xx <- rep(xx, length.out=n)
-        yy <- rep(yy, length.out=n)
-    }
-    if (!x$open) {
-        xx <- c(xx, xx[1])
-        yy <- c(yy, yy[1])
-    }
-    slope <- bezSplineSlope(xx, yy, x$stepFn)
-    len <- sqrt(slope$x^2 + slope$y^2)
-    list(x=slope$x/len, y=slope$y/len)
-}
-
-BezierNormal <- function(x) {
-    tangent <- BezierTangent(x)
-    list(x=tangent$y, y=-tangent$x)
-}
-
 ## Just start with single Bezier curve
 ## 'x' and 'y' must be length 4
 bezCurvePts <- function(x, y, stepFn=nSteps(100), range=c(0, 1)) {
-    t <- stepFn(x, y, range)
-    t2 <- t*t
-    t3 <- t2*t
-    tmatrix <- cbind(1, t, t2, t3)
-    mult <- tmatrix %*% bezMatrix
-    bx <- mult %*% x
-    by <- mult %*% y
-    cbind(x=bx, y=by)
+    if (any(is.na(range))) {
+        cbind(x=numeric(), y=numeric())
+    } else {
+        t <- stepFn(x, y, range)
+        t2 <- t*t
+        t3 <- t2*t
+        tmatrix <- cbind(1, t, t2, t3)
+        mult <- tmatrix %*% bezMatrix
+        bx <- mult %*% x
+        by <- mult %*% y
+        cbind(x=bx, y=by)
+    }
+}
+
+genRanges <- function(range, ncurves) {
+    if (is.null(range)) {
+        ranges <- matrix(0:1, nrow=2, ncol=ncurves)
+    } else {
+        ranges <- matrix(NA, nrow=2, ncol=ncurves)
+        for (i in 1:ncurves) {
+            if (range[1] < i || i == ncurves) {
+                if (i == 1 || range[1] > i - 1) {
+                    ranges[1, i] <- range[1] - (i - 1)
+                } else {
+                    ranges[1, i] <- 0
+                }
+            }
+            if (range[2] > i - 1 || i == 1) {
+                if (i == ncurves || range[2] < i) {
+                    ranges[2, i] <- range[2] - (i - 1)
+                } else {
+                    ranges[2, i] <- 1
+                }
+            }
+        }
+    }
+    ranges
 }
 
 ## Expand to Bezier spline
 ## 'x' and 'y' must be length multiple-of-three-plus-one (and the same length)
-bezSplinePts <- function(x, y, stepFn=nSteps(100), range=c(0, 1)) {
+bezSplinePts <- function(x, y, stepFn=nSteps(100), range=NULL) {
     ncurves <- (length(x) - 1) %/% 3
     if (ncurves*3 + 1 != length(x)) 
         stop("Invalid number of control points")
     ## Expand 'x' and 'y' to sets of four control points
     index <- lapply(1:ncurves, function(i) ((i-1)*3 + 1):(i*3 + 1))
+    ## 'range' determines how much of the spline to generate
+    ranges <- genRanges(range, ncurves)
     curves <- lapply(1:ncurves,
                      function(i) {
                          index <- ((i-1)*3 + 1):(i*3 + 1)
-                         if (i == 1) {
-                             bezCurvePts(x[index], y[index], stepFn, range)
+                         if (i == 1 || any(is.na(ranges[,i-1]))) {
+                             bezCurvePts(x[index], y[index],
+                                         stepFn, ranges[,i])
                          } else {
                              ## Drop first point to avoid duplication
-                             bezCurvePts(x[index], y[index], stepFn, range)[-1,]
+                             bezCurvePts(x[index], y[index],
+                                         stepFn, ranges[,i])[-1,]
                          }
                      })
     list(x=unlist(lapply(curves, function(c) c[,1])),
          y=unlist(lapply(curves, function(c) c[,2])))
 }
 
-BezierPoints <- function(x, range=c(0, 1)) {
+BezierPoints <- function(x, range=NULL) {
     xx <- convertX(x$x, "in", valueOnly=TRUE)
     yy <- convertY(x$y, "in", valueOnly=TRUE)
     nx <- length(xx)
@@ -123,6 +103,63 @@ BezierPoints <- function(x, range=c(0, 1)) {
         yy <- c(yy, yy[1])
     }
     bezSplinePts(xx, yy, x$stepFn, range)    
+}
+
+bezCurveSlope <- function(x, y, stepFn=nSteps(100), range=c(0, 1)) {
+    t <- stepFn(x, y, range)
+    t2 <- t*t
+    tmatrix <- cbind(1, t, t2)
+    mult <- tmatrix %*% derivMatrix
+    dx <- mult %*% x
+    dy <- mult %*% y
+    cbind(x=dx, y=dy)
+}
+
+bezSplineSlope <- function(x, y, stepFn=nSteps(100), range=NULL) {
+    ncurves <- (length(x) - 1) %/% 3
+    if (ncurves*3 + 1 != length(x)) 
+        stop("Invalid number of control points")
+    ## Expand 'x' and 'y' to sets of four control points
+    index <- lapply(1:ncurves, function(i) ((i-1)*3 + 1):(i*3 + 1))
+    ## 'range' determines how much of the spline to generate
+    ranges <- genRanges(range, ncurves)
+    slopes <- lapply(1:ncurves,
+                     function(i) {
+                         index <- ((i-1)*3 + 1):(i*3 + 1)
+                         if (i == 1 || any(is.na(ranges[,i-1]))) {
+                             bezCurveSlope(x[index], y[index],
+                                           stepFn, ranges[,i])
+                         } else {
+                             bezCurveSlope(x[index], y[index],
+                                           stepFn, ranges[,i])[-1,]
+                         }
+                     })
+    list(x=unlist(lapply(slopes, function(c) c[,1])),
+         y=unlist(lapply(slopes, function(c) c[,2])))
+}
+
+BezierTangent <- function(x, range=NULL) {
+    xx <- convertX(x$x, "in", valueOnly=TRUE)
+    yy <- convertY(x$y, "in", valueOnly=TRUE)
+    nx <- length(xx)
+    ny <- length(yy)
+    n <- max(nx, ny)
+    if (nx != ny) {
+        xx <- rep(xx, length.out=n)
+        yy <- rep(yy, length.out=n)
+    }
+    if (!x$open) {
+        xx <- c(xx, xx[1])
+        yy <- c(yy, yy[1])
+    }
+    slope <- bezSplineSlope(xx, yy, x$stepFn, range)
+    len <- sqrt(slope$x^2 + slope$y^2)
+    list(x=slope$x/len, y=slope$y/len)
+}
+
+BezierNormal <- function(x, range=NULL) {
+    tangent <- BezierTangent(x, range)
+    list(x=tangent$y, y=-tangent$x)
 }
 
 makeContent.Beziergrob <- function(x) {
---
+++ tests/range.R
@@ -0,0 +1,27 @@
+
+library(gridBezier)
+
+## Generate points (or tangents or normals) for only a range of Bezier spline
+
+x <- seq(.3, .7, length.out=10)
+y <- rep(c(.1, .2, .2), length.out=10)
+
+testRange <- function(r, offset) {
+    bg <- BezierGrob(x, y + offset, stepFn=nSteps(10))
+    grid.draw(bg)
+    pts <- BezierPoints(bg, r)
+    grid.circle(pts$x, pts$y, unit(.5, "mm"), "in", gp=gpar(fill="white"))
+}
+
+grid.newpage()
+testRange(NULL, 0)
+testRange(c(0, 1), .2)
+testRange(c(-.5, 0), .4)
+testRange(c(.5, 1.5), .6)
+testRange(c(1, 2), .8)
+
+grid.newpage()
+testRange(c(1.5, 2.5), 0)
+testRange(c(2, 3), .2)
+testRange(c(3, 3.5), .4)
+
